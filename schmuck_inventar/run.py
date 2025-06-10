f2986d578b9a331d020c1eef1efaaa323a164889
Mathias Zinnen <mathias.zinnen@sammlungen.io>
2025-06-10 11:37:23 +0200

    add basic postprocessing

--- schmuck_inventar/run.py
+++ schmuck_inventar/run.py
@@ -3,7 +3,7 @@ import os
 import sys
 from schmuck_inventar.detection import YoloImageDetector
 from schmuck_inventar.recognition import DummyCardRecognizer, MacOSCardRecognizer
-from schmuck_inventar.postprocessor import PostProcessor
+from schmuck_inventar.postprocessor import SchmuckPostProcessor
 import platform
 import appdirs
 from PIL import Image
@@ -13,6 +13,7 @@ from tqdm import tqdm
 
 def pipeline(input_dir, output_dir, layout_config):
     print(f"Processing files in directory: {input_dir}")
+
     app_dir = appdirs.user_data_dir("schmuck_inventar")
     detector = YoloImageDetector(resources_path=os.path.join(app_dir,"detection"))
     if platform.system() == 'Darwin':
@@ -25,30 +26,30 @@ def pipeline(input_dir, output_dir, layout_config):
     with open(layout_config, 'r') as config_file:
         config_file = yaml.safe_load(config_file)
         layout_keys = config_file['regions'].keys() 
-        header_mappings = config_file.get('header_mappings', {})
 
-    postprocessor = PostProcessor()
-    # Prepare CSV file
-    csv_file_path = os.path.join(output_dir, 'results.csv')
+    results_csv_raw = os.path.join(output_dir, 'results_raw.csv')
     os.makedirs(output_dir, exist_ok=True)
 
-    with open(csv_file_path, mode='w', newline='', encoding='utf-8') as csv_file:
+    with open(results_csv_raw, mode='w', newline='', encoding='utf-8') as csv_file:
         csv_writer = csv.DictWriter(csv_file, fieldnames=['source_file'] + list(layout_keys))
         csv_writer.writeheader()
 
-        # Process each file
         for filename in tqdm(os.listdir(input_dir)):
             file_path = os.path.join(input_dir, filename)
             image = Image.open(file_path)
             detections = detector.detect(image)
             detector.crop_and_save(detections, os.path.join(output_dir, 'images'), filename)
             results = recognizer.recognize(image, filename)
-            results = postprocessor.postprocess(results)
 
-            # Write results to CSV
+            # Write raw results to CSV
             row = {'source_file': filename}
             row.update({key: results.get(key, '') for key in layout_keys})
             csv_writer.writerow(row)
+        print(f"Raw extraction results written to {results_csv_raw}")
+    
+    final_csv_output = os.path.join(output_dir, 'results.csv')
+    postprocessor = SchmuckPostProcessor(results_csv_raw, final_csv_output)
+    postprocessor.postprocess()
 
 def main():
     parser = argparse.ArgumentParser(description="Process input directory for Schmuck Inventar.")
@@ -82,8 +83,6 @@ def main():
         print(f"Error: The directory '{input_dir}' does not exist.", file=sys.stderr)
         sys.exit(1)
 
-    print(f"Processing files in directory: {input_dir}")
-    # Add your processing logic here
 
 if __name__ == "__main__":
     main()
